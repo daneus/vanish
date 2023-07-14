231604787c318e8dbb9b1df48275261e6513497a
Ezkrman <tony.almonacid@hotmail.com>
2023-07-14 13:34:49 -0500

    Checkbox toggle logic added, pending class toggling

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,27 @@
-async function getTabId() {
+const checkbox = document.getElementById('vanish_toggle');
+
+const getTabId = async () => {
   let queryOptions = { active: true, lastFocusedWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab.id;
-}
+};
+
+(async () => {
+  const { vanish } = await chrome.storage.local.get(['vanish']);
+  if (vanish) {
+    checkbox.checked = vanish;
+  }
+})();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const checkbox = document.getElementById('vanish_toggle');
   checkbox.addEventListener('change', async () => {
+    const { vanish } = await chrome.storage.local.get(['vanish']);
+    chrome.storage.local.set({ vanish: !vanish });
+    if (!vanish) {
+      chrome.action.setBadgeText({ text: 'ON' });
+    } else {
+      chrome.action.setBadgeText({ text: '' });
+    }
     chrome.scripting.executeScript({
       target: { tabId: await getTabId() },
       files: ['/scripts/vanish-script.js'],
